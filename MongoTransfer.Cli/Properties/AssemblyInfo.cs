3a4a32a5ac5509addacce831c1cd5afc941d7628
Simon Mourier <smourier@users.noreply.github.com>
2025-01-04 12:47:52 +0100

    updated copyrights

--- MongoTransfer.Cli/Properties/AssemblyInfo.cs
+++ MongoTransfer.Cli/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MongoTransfer.Cli")]
 [assembly: AssemblyDescription("A console utility to upsert documents from a MongoDB collection to another.")]
 [assembly: AssemblyProduct("MongoTransfer")]
-[assembly: AssemblyCopyright("Copyright © 2016-2024 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2016-2025 Simon Mourier. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2451e434-4a8e-44f7-9600-b8278ade2f78")]
 [assembly: AssemblyVersion("1.0.1.0")]
